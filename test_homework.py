5238d1914a0392c9ef7e0672fcc05d780416ad91
necros <necros83@gmail.com>
2023-11-26 02:27:28 +0300

    done

--- test_homework.py
+++ test_homework.py
@@ -1,5 +1,15 @@
+import sys
 from datetime import time
 
+
+def check_time(current_time):
+    if current_time >= time(hour=22) or current_time <= time(hour=6):
+        is_dark_theme = True
+        print('Dark theme is enable')
+    else:
+        is_dark_theme = False
+        print('Dark theme is disable')
+    return is_dark_theme
 
 def test_dark_theme_by_time():
     """
@@ -7,8 +17,11 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-
     is_dark_theme = None
+
+    is_dark_theme = check_time(current_time)
+    print(is_dark_theme)
+
     assert is_dark_theme is True
 
 
@@ -26,6 +39,12 @@ def test_dark_theme_by_time_and_user_choice():
     #  но учтите что темная тема может быть включена вручную
 
     is_dark_theme = None
+    if dark_theme_enabled_by_user:
+        is_dark_theme = dark_theme_enabled_by_user
+    else:
+        check_time(current_time)
+
+
     assert is_dark_theme is True
 
 
@@ -43,10 +62,20 @@ def test_find_suitable_user():
 
     # TODO найдите пользователя с именем "Olga"
     suitable_users = None
+
+    for i in users:
+        if i['name'] == 'Olga':
+            suitable_users = i
+
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    suitable_users = []
+
+    for i, v in enumerate(users):
+        if v['age'] < 20:
+            suitable_users.append(users[i])
+
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
@@ -70,16 +99,31 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+def func_print():
+    func_name = sys._getframe(1).f_code.co_name
+    get_attr = sys._getframe(1).f_locals
+    name = (' '.join(func_name.split('_'))).title()
+    list = []
+    for attr in get_attr.values():
+        list.append(attr)
+    attr = ', '.join(list)
+    print(name, attr)
+    return f'{name} [{attr}]'
+
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = func_print()
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = func_print()
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = func_print()
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+
+
+test_readable_function()
